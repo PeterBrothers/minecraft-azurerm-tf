470fe513e27bbe4c1f9c2a30f265afbc660615c8
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-01 22:25:28 -0400

    add more resources

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ variable client_secret {}
 variable location {}
 variable environment {}
 
+variable "prefix" {
+  default = "minecraft"
+}
+
 
 provider "azurerm" {
  version = "~> 2.0.0"
@@ -24,4 +28,107 @@ resource "azurerm_resource_group" "rg" {
   tags = {
     environment = var.environment
   }
+}
+
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.2.0/24"
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+
+resource "azurerm_public_ip" "pip" {
+  name                    = "minecraft-pip"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "minecraftconfiguration"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+
+resource "azurerm_virtual_machine" "minecraftvm" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_B2s_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "minecraft"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = var.environment
+  }
+}
+
+
+data "azurerm_public_ip" "pip" {
+  name                = azurerm_public_ip.pip.name
+  resource_group_name = azurerm_virtual_machine.minecraftvm.resource_group_name
+}
+
+output "vm_public_ip_address" {
+  value = data.azurerm_public_ip.pip.ip_address
 }
